253b7c462a4e53202a3e05feef52b114e036b7f6
rwlovett <106644832+rwlovett@users.noreply.github.com>
2026-07-31 16:35:09 -0500

    Initial commit - uploading files already created
    This commit is to migrate the work already done offline.

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+﻿/*void button() {
+    var x = $(document).getElementById("span");
+    x.onclick = toggle;
+}
+
+void toggle() {
+    var y = $(document).getElementById("js-menu");
+    if (y.style.display == "none") {
+        y.style.display = "flex";
+    } else {
+        y.style.display = "none";
+    }
+}*/
